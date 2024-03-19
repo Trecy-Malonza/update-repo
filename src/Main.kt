cd2b2ff0faf5cd8ed7fecef5ab2f2804943a23c3
Trecy-Malonza <trecymalonza@gmail.com>
2024-03-19 08:23:44 +0300

    added file

---
+++ src/Main.kt
@@ -0,0 +1,28 @@
+fun main() {
+        var num1 = 50
+        var num2 = 30
+        var sum = num1 + num2
+        println(sum)
+
+        var num3 = 34
+        var num4 = 234
+        var remainder = num3 % num4
+        println(remainder)
+
+        var a = 4
+        var b = 5
+        a = a - b
+        println(a)
+
+    var d=58
+    var e=6
+    var product=d*e
+    println(product)
+
+    var g =78
+    var h = 56
+    var division =g/h
+    println(division)
+    }
+
+
